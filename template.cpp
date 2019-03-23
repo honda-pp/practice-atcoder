4b84b2b91d4071c147758d0ece87574fc61feaef
honda-keio <honda@ykw.keio.ac.jp>
2019-03-23 14:44:07 +0900

    first

---
+++ template.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define forN(n, N) for(int n=0; n<N; n++)
+#define res_out(ans) cout << ans << "\n";
+#define ll long long
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    return 0;
+}
